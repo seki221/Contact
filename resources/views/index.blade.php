4b5606cd0542a59c10e4d2d04946554be452fa6f
seki221 <pawls00.bugaboo@icloud.com>
2023-03-24 16:58:15 +0900

    c

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,67 +12,68 @@
 
 <body>
   <h1>お問い合わせ</h1><br>
-  <!-- <form action="/form.php" method="post">
+  <form action="/form.php" method="post">
     <div class="formtag">
-      <div class="label">
-        <th><label for="name">お名前<span class="asterisk">※</span></label></th>
-        <td><input type="text" id="name" name="firstname" required></td>
-        <td><input type="text" id="name" name="lastname" required></td>
-      </div>
-      <div class="label">
-        <th></th><label for="gender">性別<span class="asterisk">※</span></label>
-        <td><input type="radio" name="gender" value="男" checked="checked"> 男性</td>
-        <td><input type="radio" name="gender" value="女"> 女性</td>
-
-      </div>
-      <div class="label">
-        <th><label for="email">メールアドレス<span class="asterisk">※</span></label></th>
-        <td><input type="name" id="email" name="email" required></td>
-      </div>
-      <div class="label">
-        <label for="postcode">郵便番号<span class="asterisk">※</span></label>
-        〒<input type="text" name="postcode" pattern="\d{3}-?\d{4}" required>
-      </div>
-      <div class="label">
-        <label for="address">住所<span class="asterisk">※</span></label>
-        <input type="text" id="address" name="address">
-      </div>
-      <div class="label">
-        <label for="email">メールアドレス<span class="asterisk">※</span></label>
-        <input type="name" id="email" name="email">
-      </div>
-      <div class="label">
-        <th><label for="message">ご意見</label></th>
-        <td><textarea required></textarea></td>
-      </div>
-      <button type="submit" name="action" value="submit">確認</button>
+      <table>
+        <tr>
+          <th><label for="name">お名前<span class="asterisk">※</span></label></th>
+          <td><input type="text" id="name" name="firstname" class="hugename" required></td>
+          <td><input type="text" id="name" name="lastname" class="hugename" required></td>
+        </tr>
+        <tr>
+          <th><label for="gender">性別<span class="asterisk">※</span></label></th>
+          <td><input type="radio" name="gender" value="男" checked="checked" style="transform:scale(2.0);"><label for="gender">男性</label></td>
+          <td><input type="radio" name="gender" value="女"> 女性</td>
+        </tr>
+        <tr>
+          <th><label for="email">メールアドレス<span class="asterisk">※</span></label></th>
+          <td class="s"><input type="name" id="email" name="email" required></td>
+        </tr>
+        <tr>
+          <th><label for="postcode">郵便番号<span class="asterisk">※</span></label></th>
+          <td class="s"><q><input type="text" name="postcode" pattern="\d{3}-?\d{4}" required></q></td>
+        </tr>
+        <tr>
+          <th><label for="address">住所<span class="asterisk">※</span></label></th>
+          <td class="s"><input type="text" id="address" name="address"></td>
+        </tr>
+        <tr>
+          <th><label for="text">建物名</label></th>
+          <td class="s"><input type="text" name="building" class="text"></td>
+        </tr>
+        <tr>
+          <th><label for="message">ご意見</label></th>
+          <td class="s"><textarea class="textarea" required></textarea></td>
+        </tr>
+      </table>
     </div>
-  </form> -->
-
-  <!-- <form class="contact" action="index.html" method="post">
-      <dl>
-        <dt>お名前</dt>
-        <dd><input type="text" name="name" class="name"></dd>
-        <dt>性別</dt>
-        <dd><input type="ragio" name="name" class="name"></dd>
-        <dt>性別<span class="asterisk">※</span></td>
-        <dd><input type="radio" name="gender" value="男" checked="checked"> 男性<input type="radio" name="gender" value="女"> 女性</dd>
-        <dt>メールアドレス</dt>
-        <dd><input type="email" name="email" class="email"></dd>
-        <dt>郵便番号</dt>
-        <dd><input type="text" name="postcode" pattern="\d{3}-?\d{4}" required></dd>
-        <dt>住所</dt>
-        <dd><input type="text" id="address" name="address"></dd>
-        <dt>建物名</dt>
-        <dd>
-          <input type="text" name="building_name" value="建物名" class="text"></dd>
-        <dt>ご意見</dt>
-        <dd><textarea name="message" class="message"></textarea></dd>
-      </dl>
-      <button type="submit" class="btn">送信</button>
-      <a>修正する</a>
-    </form> -->
+    <button type="submit" name="action" value="submit">確認</button>
+  </form>
   <form class="contact" action="index.html" method="post">
+    <dl>
+      <dt>お名前</dt>
+      <dd><input type="text" name="name" class="name"></dd>
+      <dt>性別</dt>
+      <dd><input type="ragio" name="name" class="name"></dd>
+      <dt>性別<span class="asterisk">※</span></td>
+      <dd><input type="radio" name="gender" value="男" checked="checked"> 男性<input type="radio" name="gender" value="女"> 女性</dd>
+      <dt>メールアドレス</dt>
+      <dd><input type="email" name="email" class="email"></dd>
+      <dt>郵便番号</dt>
+      <dd><input type="text" name="postcode" pattern="\d{3}-?\d{4}" required></dd>
+      <dt>住所</dt>
+      <dd><input type="text" id="address" name="address"></dd>
+      <dt>建物名</dt>
+      <dd>
+        <input type="text" name="building_name" class="text">
+      </dd>
+      <dt>ご意見</dt>
+      <dd><textarea name="message" class="message"></textarea></dd>
+    </dl>
+    <button type="submit" class="btn">送信</button>
+
+  </form>
+  <!-- <form class="contact" action="index.html" method="post">
     <div class="flex_box">
       <div class="flex_form-item">
         <div class="box_left">
@@ -101,7 +102,7 @@
         </div>
       </div>
     </div>
-  </form>
+  </form> -->
   <button type="submit" class="btn">送信</button>
 </body>
 
